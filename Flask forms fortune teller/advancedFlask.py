5dc39d884a74cd5af7fdc2ed2eddb12b336fcdbe
beerisb24-meet <beeribersh@gmail.com>
2024-07-17 22:28:36 +0300

    advanced flask forms fortune teller

--- Flask forms fortune teller/advancedFlask.py
+++ Flask forms fortune teller/advancedFlask.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 import random
-app=Flask(__name__ ,template_folder='templates')
+app=Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def home():
-	if request.method=='GET':
-		return render_template("home.html")
-	else:
-		birthMonth=request.form["birthMonth"]
-		return redirect(url_for('fortune', bm=birthMonth))
+	return render_template("home.html")
 
-@app.route("/fortune/<bm>")
-def fortune(bm):
+@app.route("/fortune")
+def fortune():
 	fortunes=["Something good will happen to you exactly at 3:17 pm tomorrow", 
 	"You will discover something lifechanging in the next week",
 	"Remember to try again if you fail", 
@@ -22,13 +18,8 @@ def fortune(bm):
 	"Don't get mad on Monday",
 	"Try to finish all your work as soon as possible",
 	"You will go through something difficult on Sunday"]
-	bmLength=len(bm)
-	if bmLength>len(fortunes):
-		monthFortune=fortunes[9]
-	else:
-		monthFortune=fortunes[bmLength-1]	
 
-	return render_template("fortune.html", yourFortune=monthFortune)
+	return render_template("fortune.html", yourFortune=fortunes[random.randint(0,9)])
 
 if __name__ == '__main__':
 	app.run(debug=True)
