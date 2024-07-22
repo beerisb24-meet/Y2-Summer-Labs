d92a65d2e33bbe64184162c920b94a7e0f1500ea
beerisb24-meet <beeribersh@gmail.com>
2024-07-22 17:43:25 +0300

    authentication lab (lab 9)

--- Authentication-Lab/app.py
+++ Authentication-Lab/app.py
@@ -12,39 +12,62 @@ firebaseConfig = {"apiKey": "AIzaSyAIVFwO8DAcNvTATZABAUGDPb1YWJgn3dU",
 "messagingSenderId": "673402144009",
 "appId": "1:673402144009:web:830fa7bc802308d4967fd8",
 "measurementId": "G-GC79GPDNRS",
-"databaseURL":""
+"databaseURL":"https://authenticationlab-7257f-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
+db=firebase.database()
 
 @app.route('/', methods=['GET', 'POST'])
 def signup():
 	if request.method=='GET':
 		return render_template('signup.html')
 	else:
+		#userId=login_session['user']['localId']
 		email=request.form['email']
 		password=request.form['password']
-		#try:
-		login_session['user'] = auth.create_user_with_email_and_password(email, password)
-		return redirect(url_for('/home'))
-		#except:
-		#	error = "Something went wrong. Try again"
-		#	return render_template("signup.html",error=error)
+		#full_name=request.form['full_name']
+		#username=request.form['username']
+		#user={"email":email, "password":password, "full_name":full_name, "username":username}
+		#db.child("Users").child(userId).set(user)
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+
+			return redirect(url_for('home'))
+		except:
+			error = "Something went wrong. Try again"
+			return render_template("signup.html",error=error)
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
 	if request.method=='GET':
 		return render_template('signin.html')
 	else:
-		return render_template('signin.html')
+		email=request.form['email']
+		password=request.form['password']
+		try:
+			login_session['user']=auth.sign_in_with_email_and_password(email, password)
+			return render_template('home.html')
+		except:
+			error="Something went wrong. try again."
+			return render_template('signin.html', error=error)
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 	if request.method=='GET':
 		return render_template('home.html')
 	else:
-		login_session['quotes']=[]
+		quote=request.form['quote']
+		login_session['quotes']+=[quote]
+		return redirect(url_for('thanks'))
+
+@app.route('/signout')
+def signout():
+	login_session['user']=None
+	auth.current_user=None
+	return redirect(url_for('signin'))
 
 @app.route('/thanks')
 def thanks():
@@ -52,7 +75,8 @@ def thanks():
 
 @app.route('/display')
 def display():
-	return render_template('display.html')
+	quotes=login_session['quotes']
+	return render_template('display.html',quotes=quotes)
 
 
 if __name__ == '__main__':
